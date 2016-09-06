5b76312681086dea5edadd6789e6c56a20d62502
AviWaiz <avi.waizman@gmail.com>
2016-09-06 08:34:53 -0700

    pre modal push

--- frontend/beardstagram.jsx
+++ frontend/beardstagram.jsx
@@ -1,6 +1,7 @@
 //React
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
 //Components
 import Root from './components/root';
 //Actions
@@ -35,7 +36,7 @@ document.addEventListener('DOMContentLoaded', () => {
   } else {
     window.store = store = configureStore();
   }
-
+  Modal.setAppElement(document.body);
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store}/>, root);
 });
