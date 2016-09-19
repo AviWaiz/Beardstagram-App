755d9093a338dc9f360500532a6107e472beae47
AviWaiz <avi.waizman@gmail.com>
2016-09-18 21:38:09 -0700

    removed redux logger

--- frontend/middleware/root_middleware.js
+++ frontend/middleware/root_middleware.js
@@ -3,14 +3,12 @@ import SessionMiddleware from '../middleware/session_middleware';
 import PhotosMiddleware from '../middleware/photos_middleware';
 import UserMiddleware from '../middleware/user_middleware';
 import SearchMiddleware from '../middleware/search_middleware';
-import logger from 'redux-logger';
 
 const RootMiddleware = applyMiddleware(
   SessionMiddleware,
   PhotosMiddleware,
   UserMiddleware,
-  SearchMiddleware,
-  logger()
+  SearchMiddleware
 );
 
 export default RootMiddleware;
